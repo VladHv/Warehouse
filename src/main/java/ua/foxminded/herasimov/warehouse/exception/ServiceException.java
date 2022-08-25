3388eb73f8696d7e3f59e80f53bacbd58d993d16
Vlad Herasimov <vlad9880@gmail.com>
2022-08-26 02:05:08 +0300

    added ServiceException

---
+++ src/main/java/ua/foxminded/herasimov/warehouse/exception/ServiceException.java
@@ -0,0 +1,8 @@
+package ua.foxminded.herasimov.warehouse.exception;
+
+public class ServiceException extends RuntimeException{
+
+    public ServiceException(String message) {
+        super(message);
+    }
+}
